29a983278de8b10eb1547438682125cebbda256a
VitinLigo <vdrf5649@gmail.com>
2026-05-10 12:16:28 -0300

    BeeCrowd

--- src/Bee1006.java
+++ src/Bee1006.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
-public class Bee1005 {
+public class Bee1006 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         double A = sc.nextDouble();
         double B = sc.nextDouble();
-        double media = (A * 3.5 + B * 7.5) / 11;
+        double C = sc.nextDouble();
+        double media = (A * 2 + B * 3 + C * 5) / 10;
 
-        System.out.println(String.format("MEDIA = %.5f", Math.min(media, 10.0)));
+        System.out.println(String.format("MEDIA = %.1f", Math.min(media, 10)));
     }
 }
